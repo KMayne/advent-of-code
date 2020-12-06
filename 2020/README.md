52db461a9009d5ac4c4e36c0bcc1694ee0c657ef
Kian Mayne <git@kianmayne.co.uk>
2020-12-06 21:19:52 +0000

    Added day5 + README

---
+++ 2020/README.md
@@ -0,0 +1,5 @@
+# 2020
+
+These are my solutions to 2020's Advent of Code.
+
+I didn't want to spend ages on it this year so I've gone with JavaScript and I'm not being too fussy about optimisations.
---
+++ 2020/day5.mjs
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+import { loadDayLines } from './lib.mjs';
+
+async function day5() {
+    const inputLines = await loadDayLines(5);
+    const presentSeatIds = inputLines.map(seatString => seatStringToId(seatString));
+    const maxSeatId = presentSeatIds.reduce((maxSeatId, seatId) => Math.max(seatId, maxSeatId), 0);
+    console.log(`Max seat ID = ${maxSeatId}`);
+    const presentSeatSet = new Set(presentSeatIds);
+    const missingSeats = [];
+    for (let i = 0; i <= maxSeatId; i++) {
+        if (!presentSeatSet.has(i)) {
+            missingSeats.push(i);
+        }
+    }
+    const mySeat = missingSeats.find(seatId => presentSeatSet.has(seatId - 1) && presentSeatSet.has(seatId + 1));
+    console.log(`My seat ID = ${mySeat}`);
+}
+
+function getSeat(seatString) {
+    const rowNum = parseInt(seatString.slice(0, 7).replace(/F/g, '0').replace(/B/g, '1'), 2);
+    const colNum = parseInt(seatString.slice(7, 10).replace(/L/g, '0').replace(/R/g, '1'), 2);
+    return [rowNum, colNum];
+}
+
+function getSeatId(row, col) {
+    return row * 8 + col;
+}
+
+function seatStringToId(seatString) {
+    const [row, col] = getSeat(seatString);
+    return getSeatId(row, col);
+}
+
+
+day5().then(() => {});
